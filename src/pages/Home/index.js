2976ebe757ec27c356bd40fd0d4c3af69b7043ff
Justin <me@justinle.page>
2020-05-12 21:34:46 -0700

    Home page component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 
+import Content from '../../components/Content';
 import Sidebar from '../../components/Sidebar';
 import './style.css';
 
 export default function Home() {
-  return (
-    <div>
-    <Sidebar />
-      {/* <Navbar bg="light" id="sidebar">
-        <Navbar.Brand className="font-weight-bold" href="#home">
-          BZY KAI
-        </Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link href="#features">CONTACT</Nav.Link>
-          <Nav.Link href="#pricing">PRICING</Nav.Link>
-          <Nav.Link href="#pricing">PREVIOUS WORKS</Nav.Link>
-        </Nav>
-      </Navbar> */}
-    </div>
-  );
+    return (
+        <div>
+            <Sidebar />
+            <Content />
+        </div>
+    );
 }
