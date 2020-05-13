bd76b00727c3ab3d71d356c116e8393c1c6651fb
Justin <me@justinle.page>
2020-05-12 21:32:53 -0700

    Content component

---
+++ src/components/Content/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GridGallery from '../GridGallery';
+import Shop from '../Shop';
+import './style.css';
+
+export default function Content() {
+    return (
+        <div className="content-wrapper">
+            <GridGallery />
+        </div>
+    );
+}
