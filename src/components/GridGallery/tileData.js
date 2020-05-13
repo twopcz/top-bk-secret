5c03f258e7e823f6d56783cd551331b4b4104257
Justin <me@justinle.page>
2020-05-12 21:33:43 -0700

    picture data

---
+++ src/components/GridGallery/tileData.js
@@ -0,0 +1,39 @@
+import Lakers from './images/1s-lakers.JPG';
+import Yeezys from './images/700s.jpg';
+import Goku from './images/goku.jpg';
+import KawsVans2 from './images/kaws-vans-2.JPG';
+import KawsVans3 from './images/kaws-vans-3.jpg';
+import KawsVans from './images/kaws-vans.jpg';
+import Kobe from './images/kobe.JPG';
+import Opm from './images/opm.jpg';
+import Opm2 from './images/opm2.JPG';
+import RoseVans2 from './images/rose-vans-2.jpg';
+import RoseVans from './images/rose-vans.JPG';
+import RoseWave from './images/rose-wave-vans.jpg';
+
+const tileData = [
+    {
+        img: Lakers
+    },
+    {
+        img: Yeezys
+    },
+    {
+        img: Goku
+    },
+
+    {
+        img: KawsVans3
+    },
+    {
+        img: Opm2
+    },
+    {
+        img: RoseVans
+    },
+    {
+        img: Kobe
+    }
+];
+
+export default tileData;
