d41525a30816534f332874e88e756aa6cedf3276
Justin <me@justinle.page>
2020-05-21 01:35:47 -0700

    New Gallery component

---
+++ src/components/Gallery/imageData.js
@@ -0,0 +1,40 @@
+import Goku from './images/goku-greeks-800.png';
+import Jordans from './images/laker-unions-800.png';
+import Kaws from './images/kaws-vans-800.png';
+import Kobe from './images/kobe-pic-800.png';
+import Opm from './images/opm-yeezy-800.png';
+import Red from './images/red-yeezy-800.png';
+import Rose from './images/rose-vans-800.png';
+
+const imageData = [
+    {
+        img: Jordans,
+        alt: 'laker-unions'
+    },
+    {
+        img: Goku,
+        alt: 'goku-greeks'
+    },
+    {
+        img: Red,
+        alt: 'red-yeezy-700s'
+    },
+    {
+        img: Opm,
+        alt: 'opm-powerphase'
+    },
+    {
+        img: Kaws,
+        alt: 'kaws-vans'
+    },
+    {
+        img: Rose,
+        alt: 'rose-vans'
+    },
+    {
+        img: Kobe,
+        alt: 'kobe-portait'
+    }
+];
+
+export default imageData;
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,51 +1,21 @@
+import './style.css';
+import imageData from './imageData';
 import React from 'react';
 import { Row } from 'react-bootstrap';
-import lakers from './union-lakers.jpg';
 
 export default function Gallery() {
     return (
-        <Row className="h-100 flex-nowrap">
-            <div>
-                <img
-                    className="img-fluid"
-                    src={lakers}
-                    alt="custom-lakers"
-                ></img>
-            </div>
-            <div>
-                <img
-                    className="img-fluid"
-                    src={lakers}
-                    alt="custom-lakers"
-                ></img>
-            </div>
-            <div>
-                <img
-                    className="img-fluid"
-                    src={lakers}
-                    alt="custom-lakers"
-                ></img>
-            </div>
-            <div>
-                <img
-                    className="img-fluid"
-                    src={lakers}
-                    alt="custom-lakers"
-                ></img>
-            </div>
-            <div>
-                <img
-                    className="img-fluid"
-                    src={lakers}
-                    alt="custom-lakers"
-                ></img>
-            </div>
-            <div>
-                <img
-                    className="img-fluid"
-                    src={lakers}
-                    alt="custom-lakers"
-                ></img>
+        <Row>
+            <div className="gallery d-flex flex-column flex-md-row flex-nowrap">
+                {imageData.map((image, index) => (
+                    <div className="gallery-item" key={`gallery-${index}`}>
+                        <img
+                            className="gallery-image"
+                            src={image.img}
+                            alt={image.alt}
+                        ></img>
+                    </div>
+                ))}
             </div>
         </Row>
     );
