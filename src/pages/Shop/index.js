71720b2aa976d00bd6362861f23da536641b3cc0
Justin <me@justinle.page>
2020-05-21 15:47:07 -0700

    Updated Shop Page

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,30 +1,20 @@
 import './style.css';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import React from 'react';
 import Countdown from 'react-countdown';
 import Union from './union1s.png';
 
 export default function Shop() {
     return (
-        <Container fluid className="h-100">
-            <Row className="h-100">
-                <div className="my-auto mx-auto">
-                    <div className="d-flex flex-column">
-                        <Col md={8} className="text-center mx-auto">
-                            <img
-                                src={Union}
-                                className="img-fluid"
-                                alt="laker-unions"
-                            ></img>
-                        </Col>
-                        <Col md={8} className="text-center mx-auto">
-                            <div className="counter">
-                                <Countdown date={new Date(1590346800000)} />
-                            </div>
-                        </Col>
-                    </div>
+        <Col className="my-auto d-flex flex-column justify-content-center">
+            <Col md={6} className="text-center mx-auto">
+                <img src={Union} className="img-fluid" alt="laker-unions"></img>
+            </Col>
+            <Col md={6} className="text-center mx-auto">
+                <div className="counter">
+                    <Countdown date={new Date(1590346800000)} />
                 </div>
-            </Row>
-        </Container>
+            </Col>
+        </Col>
     );
 }
