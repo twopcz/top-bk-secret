32294c528c8aa02c32ba59168058cbe3ff2d6658
Justin <me@justinle.page>
2020-05-21 14:43:10 -0700

    Added container

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,22 +1,24 @@
 import './style.css';
 import imageData from './imageData';
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 export default function Gallery() {
     return (
-        <Row className="h-100">
-            <div className="gallery d-flex flex-column flex-md-row flex-nowrap my-auto">
-                {imageData.map((image, index) => (
-                    <div className="gallery-item" key={`gallery-${index}`}>
-                        <img
-                            className="gallery-image"
-                            src={image.img}
-                            alt={image.alt}
-                        ></img>
-                    </div>
-                ))}
-            </div>
-        </Row>
+        <Container fluid className="h-100">
+            <Row className="h-100 justify-content-center">
+                <div className="gallery d-flex flex-column flex-md-row flex-nowrap my-auto">
+                    {imageData.map((image, index) => (
+                        <div className="gallery-item" key={`gallery-${index}`}>
+                            <img
+                                className="gallery-image"
+                                src={image.img}
+                                alt={image.alt}
+                            ></img>
+                        </div>
+                    ))}
+                </div>
+            </Row>
+        </Container>
     );
 }
