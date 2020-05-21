efaa933e7bfb567dbff3e1c21f098f743e38ec15
Justin <me@justinle.page>
2020-05-21 01:37:28 -0700

    New gallery implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import React from 'react';
@@ -7,8 +7,9 @@ import React from 'react';
 function App() {
     return (
         <Router>
-            {/* <Route exact path="/" component={Home} /> */}
-            <Route path="/" component={Shop} />
+            <Route exact path="/" component={Home} />
+            <Route path="/shop" component={Shop} />
+            {/* <Route path="/terms" component={Terms} /> */}
         </Router>
     );
 }
--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Gallery from '../Gallery';
 import GridGallery from '../GridGallery';
-import Shop from '../Shop';
+// import Shop from '../Shop';
 import './style.css';
 
 export default function Content() {
     return (
         <div className="content-wrapper">
-            <GridGallery />
+            {/* <GridGallery /> */}
+            <Gallery />
         </div>
     );
 }
