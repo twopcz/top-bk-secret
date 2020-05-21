16b5724cb1adf1e3e5d8a196f14d344fca2b22b2
Justin <me@justinle.page>
2020-05-21 15:46:56 -0700

    Updated Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import Content from '../../components/Content';
 import './style.css';
+import { Col } from 'react-bootstrap';
+import Gallery from '../../components/Gallery';
+import React from 'react';
 
 export default function Home() {
     return (
-        <div className="home-page h-100">
-            <Content />
-        </div>
+        <Col md={11} className="px-0 px-md-4">
+            <Gallery />
+        </Col>
     );
 }
