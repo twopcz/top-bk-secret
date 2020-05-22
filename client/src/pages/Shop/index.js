681dd0f5b53337351b377669aa4792f7d9f29150
Justin <me@justinle.page>
2020-05-22 16:12:51 -0700

    Removed terms

--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -6,7 +6,7 @@ import Union from './union1s.png';
 
 export default function Shop() {
     return (
-        <Col className="my-auto d-flex flex-column justify-content-center">
+        <Col className="my-auto d-flex flex-column justify-content-center pt-5 pt-md-0 mt-5">
             <Col md={6} className="text-center mx-auto">
                 <img src={Union} className="img-fluid" alt="laker-unions"></img>
             </Col>
@@ -18,7 +18,7 @@ export default function Shop() {
                     <div className="text-center shoe-title pb-2">
                         <h2>Air Jordan 1 Union Lakers</h2>
                     </div>
-                    <div className="shoe-description">
+                    {/* <div className="shoe-description">
                         Made to order.
                         <br></br>Cost includes customization service done on a
                         100% authentic pair of Air Jordan 1s, which will be
@@ -31,7 +31,7 @@ export default function Shop() {
                         and signature confirmation.
                         <br></br>All sales are final. No cancellations, refunds,
                         or exchanges once order is processed.
-                    </div>
+                    </div> */}
                 </div>
             </Col>
         </Col>
