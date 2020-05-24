bc933b7a5c154b531019a1b5ac0595d553741bac
Justin <me@justinle.page>
2020-05-24 09:42:49 -0700

    adjusted col size

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -6,12 +6,8 @@ import Profile from './profile.jpg';
 export default function Contact() {
     return (
         <Col className="my-auto d-flex flex-column flex-md-row justify-content-center pt-5 pt-md-0 px-0">
-            <Col md={6} className="text-center mx-auto px-0">
-                <img
-                    src={Profile}
-                    className="img-fluid profile-pic"
-                    alt="profile"
-                ></img>
+            <Col md={4} className="text-center mx-auto px-0">
+                <img src={Profile} className="img-fluid" alt="profile"></img>
             </Col>
             <Col md={6} className="mx-auto my-auto">
                 <ContactForm />
