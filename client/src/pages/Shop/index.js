50be721946c0a228329ac2355bf68c1c343de276
Justin <me@justinle.page>
2020-05-23 21:48:53 -0700

    added countdown renderer

--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -5,6 +5,29 @@ import Countdown from 'react-countdown';
 import Union from './union1s.png';
 
 export default function Shop() {
+    const renderer = ({ hours, minutes, seconds, completed }) => {
+        if (completed) {
+            return (
+                <span>
+                    <a
+                        href="https://shop.bzy-kai.com/"
+                        className="text-dark"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        https://shop.bzy-kai.com/
+                    </a>
+                </span>
+            );
+        } else {
+            return (
+                <span>
+                    {hours}:{minutes}:{seconds}
+                </span>
+            );
+        }
+    };
+
     return (
         <Col className="my-auto d-flex flex-column justify-content-center pt-5 pt-md-0 mt-5">
             <Col md={6} className="text-center mx-auto">
@@ -12,26 +35,10 @@ export default function Shop() {
             </Col>
             <Col md={6} className="mx-auto">
                 <div className="text-center counter">
-                    <Countdown date={new Date(1590346800000)} />
-                </div>
-                <div className="py-3">
-                    <div className="text-center shoe-title pb-2">
-                        <h2>Air Jordan 1 Union Lakers</h2>
-                    </div>
-                    {/* <div className="shoe-description">
-                        Made to order.
-                        <br></br>Cost includes customization service done on a
-                        100% authentic pair of Air Jordan 1s, which will be
-                        provided.
-                        <br></br>Shoes will be customized by a combination of
-                        hand painting, airbrushing, sewing, and dyeing.
-                        <br></br>Please allow 6-10 weeks for completion and
-                        delivery after order is placed.
-                        <br></br>Product will be shipped via USPS, with tracking
-                        and signature confirmation.
-                        <br></br>All sales are final. No cancellations, refunds,
-                        or exchanges once order is processed.
-                    </div> */}
+                    <Countdown
+                        date={new Date(1590346800000)}
+                        renderer={renderer}
+                    />
                 </div>
             </Col>
         </Col>
