f0bbaf1abf588d318730a084dd109c1dec75af36
Justin <me@justinle.page>
2020-05-23 22:36:20 -0700

    changed profile pic again

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,12 +1,12 @@
 import { Col } from 'react-bootstrap';
 import ContactForm from '../../components/ContactForm';
 import React from 'react';
-import Profile from './profile-1200.png';
+import Profile from './profile.jpg';
 
 export default function Contact() {
     return (
         <Col className="my-auto d-flex flex-column flex-md-row justify-content-center pt-5 pt-md-0 px-0">
-            <Col md={6} className="text-center mx-auto px-0">
+            <Col md={6} className="text-center mx-auto pt-5 pt-md-0 px-0">
                 <img src={Profile} className="img-fluid" alt="profile"></img>
             </Col>
             <Col md={6} className="mx-auto my-auto">
