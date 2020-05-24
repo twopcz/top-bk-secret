1aa12d345b10611711b53b31f768dcd0a30ec1f0
Justin <me@justinle.page>
2020-05-23 22:26:48 -0700

    changed to flex-row

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,15 +1,15 @@
 import { Col } from 'react-bootstrap';
 import ContactForm from '../../components/ContactForm';
 import React from 'react';
-import Profile from './profile.jpg';
+import Profile from './profile-800.png';
 
 export default function Contact() {
     return (
-        <Col className="my-auto d-flex flex-column justify-content-center pt-5 pt-md-0">
-            <Col md={6} className="text-center mx-auto">
+        <Col className="my-auto d-flex flex-column flex-md-row justify-content-center pt-5 pt-md-0 px-0">
+            <Col md={6} className="text-center mx-auto px-0">
                 <img src={Profile} className="img-fluid" alt="profile"></img>
             </Col>
-            <Col md={6} className="mx-auto">
+            <Col md={6} className="mx-auto my-auto">
                 <ContactForm />
             </Col>
         </Col>
