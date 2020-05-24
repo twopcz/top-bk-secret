ff592dc82089bd328c1f4ccd4535011e4fc6a34f
Justin <me@justinle.page>
2020-05-23 20:39:05 -0700

    Added profile pic

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,13 +1,13 @@
 import { Col } from 'react-bootstrap';
 import ContactForm from '../../components/ContactForm';
 import React from 'react';
-import Union from './union1s.png';
+import Profile from './profile.jpg';
 
 export default function Contact() {
     return (
         <Col className="my-auto d-flex flex-column justify-content-center">
             <Col md={6} className="text-center mx-auto">
-                {/* <img src={Union} className="img-fluid" alt="laker-unions"></img> */}
+                <img src={Profile} className="img-fluid" alt="profile"></img>
             </Col>
             <Col md={6} className="mx-auto">
                 <ContactForm />
