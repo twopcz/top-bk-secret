cb8691789ef383f37d998a367bae4ffe88cfd014
Justin <me@justinle.page>
2020-09-11 14:47:41 -0700

    added new images

--- client/src/components/Gallery/imageData.js
+++ client/src/components/Gallery/imageData.js
@@ -5,6 +5,12 @@ import Kobe from './images/kobe-pic-800.png';
 import Opm from './images/opm-yeezy-800.png';
 import Red from './images/red-yeezy-800.png';
 import Rose from './images/rose-vans-800.png';
+// new images upload 09-11-20
+import Yellow from './images/lakers-unions-yellow-800.png';
+import Air from './images/solescence-800.png';
+import Kobe3 from './images/lakers-threes-800.png';
+import Unc from './images/unc-800.png';
+import Tag from './images/bzy3-tag-800.png';
 
 const imageData = [
     {
@@ -34,6 +40,26 @@ const imageData = [
     {
         img: Kobe,
         alt: 'kobe-portait'
+    },
+    {
+        img: Yellow,
+        alt: 'union-yellow-bg'
+    },
+    {
+        img: Air,
+        alt: 'bzy-air-ones'
+    },
+    {
+        img: Kobe3,
+        alt: 'vintage-kobe-threes'
+    },
+    {
+        img: Unc,
+        alt: 'vintage-unc-ones'
+    },
+    {
+        img: Tag,
+        alt: 'bzy-air-tag'
     }
 ];
 
