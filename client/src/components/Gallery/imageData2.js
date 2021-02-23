3a41fd002339bc9dbf148713b0cf9086dbd49c1c
Justin <me@justinle.page>
2021-02-22 19:40:11 -0800

    new pictures/order

---
+++ client/src/components/Gallery/imageData2.js
@@ -0,0 +1,80 @@
+import One from './images2/1.jpeg';
+import Two from './images2/2.jpeg';
+import Three from './images2/3.jpeg';
+import Four from './images2/4.jpeg';
+import Five from './images2/5.jpeg';
+import Six from './images2/6.jpeg';
+import Seven from './images2/7.jpeg';
+import Eight from './images2/8.jpeg';
+import Nine from './images2/9.jpeg';
+import Ten from './images2/10.jpeg';
+import Eleven from './images2/11.jpeg';
+import Twelve from './images2/12.jpeg';
+import Thirteen from './images2/13.jpeg';
+import Fourteen from './images2/14.jpeg';
+import Fifteen from './images2/15.jpeg';
+
+const imageData2 = [
+    {
+        img: One,
+        alt: `jordan-one-lakers-marble`
+    },
+    {
+        img: Two,
+        alt: `jordan-one-bredtoes-aged`
+    },
+    {
+        img: Three,
+        alt: `jordan-one-cement`
+    },
+    {
+        img: Four,
+        alt: `jordan-one-lakers-snakeskin`
+    },
+    {
+        img: Five,
+        alt: `jordan-one-unc-aged`
+    },
+    {
+        img: Six,
+        alt: `vans-kaws`
+    },
+    {
+        img: Seven,
+        alt: `jordan-four-firered-aged`
+    },
+    {
+        img: Eight,
+        alt: `jordan-three-lakers-aged`
+    },
+    {
+        img: Nine,
+        alt: `zoom-freak-goku`
+    },
+    {
+        img: Ten,
+        alt: `jordan-five-firered-aged`
+    },
+    {
+        img: Eleven,
+        alt: `yeezy-500-blood`
+    },
+    {
+        img: Twelve,
+        alt: `vans-rose`
+    },
+    {
+        img: Thirteen,
+        alt: `louis-vuitton-murakami`
+    },
+    {
+        img: Fourteen,
+        alt: `kobe-248-painting`
+    },
+    {
+        img: Fifteen,
+        alt: `jordan-one-lakers-yellow`
+    }
+];
+
+export default imageData2;
