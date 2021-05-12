9bc02ce32a0321dacff525b38d8b31ede2b6f1d2
Justin <me@justinle.page>
2021-05-11 20:56:59 -0700

    lookbook page

---
+++ client/src/pages/Lookbook/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+import { Col } from 'react-bootstrap';
+import Gallery from '../../components/Gallery';
+import Pictures from '../../assets/lookbookData';
+import React from 'react';
+
+export default function Home() {
+    return (
+        <Col md={11} className="px-0 px-md-4 pt-5 pt-md-0">
+            <Gallery images={Pictures} />
+        </Col>
+    );
+}
