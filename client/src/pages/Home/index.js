e6db4181843ab4c9d57f618a655f5af5db5890ba
Justin <me@justinle.page>
2021-05-11 20:57:41 -0700

    update imports, file structure

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import './style.css';
 import { Col } from 'react-bootstrap';
 import Gallery from '../../components/Gallery';
+import Portfolio from '../../assets/portfolioData';
 import React from 'react';
 
 export default function Home() {
     return (
         <Col md={11} className="px-0 px-md-4 pt-5 pt-md-0">
-            <Gallery />
+            <Gallery images={Portfolio} />
         </Col>
     );
 }
