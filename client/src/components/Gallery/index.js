61a3967d5f7396e24e64eab580b40979e838742b
Justin <me@justinle.page>
2021-05-11 20:51:26 -0700

    passed props

--- client/src/components/Gallery/index.js
+++ client/src/components/Gallery/index.js
@@ -1,15 +1,13 @@
 import './style.css';
-// import imageData from './imageData';
-import imageData2 from './imageData2';
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
-export default function Gallery() {
+export default function Gallery(props) {
     return (
         <Container fluid className="h-100">
             <Row className="h-100 justify-content-center">
                 <div className="gallery d-flex flex-column flex-md-row flex-nowrap my-auto">
-                    {imageData2.map((image, index) => (
+                    {props.images.map((image, index) => (
                         <div className="gallery-item" key={`gallery-${index}`}>
                             <img
                                 className="gallery-image"
