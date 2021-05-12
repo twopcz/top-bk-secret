94d01e58e7360d6415e2f5ed13e95241e1ebeab9
Justin <me@justinle.page>
2021-05-11 21:27:36 -0700

    updated function name

--- client/src/pages/Lookbook/index.js
+++ client/src/pages/Lookbook/index.js
@@ -4,7 +4,7 @@ import Gallery from '../../components/Gallery';
 import Pictures from '../../assets/lookbookData';
 import React from 'react';
 
-export default function Home() {
+export default function Lookbook() {
     return (
         <Col md={11} className="px-0 px-md-4 pt-5 pt-md-0">
             <Gallery images={Pictures} />
