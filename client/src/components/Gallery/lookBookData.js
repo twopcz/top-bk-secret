621ef06c2f3665813bba638faad1cbd5a5aac1a8
Justin <me@justinle.page>
2021-05-11 19:13:26 -0700

    look book data

---
+++ client/src/components/Gallery/lookBookData.js
@@ -0,0 +1,60 @@
+import One from './lookbook/1.jpg';
+import Two from './lookbook/2.jpg';
+import Three from './lookbook/3.jpg';
+import Four from './lookbook/4.jpg';
+import Five from './lookbook/5.jpg';
+import Six from './lookbook/6.jpg';
+import Seven from './lookbook/7.jpg';
+import Eight from './lookbook/8.jpg';
+import Nine from './lookbook/9.jpg';
+import Ten from './lookbook/10.jpg';
+import Eleven from './lookbook/11.png';
+
+const lookBookData = [
+    {
+        img: One,
+        alt: `jordan-one-taupe-grey`
+    },
+    {
+        img: Two,
+        alt: `jordan-one-union-royal-shadow`
+    },
+    {
+        img: Three,
+        alt: `jordan-one-travis-scott-university-blue`
+    },
+    {
+        img: Four,
+        alt: `jordan-one-ageing-service`
+    },
+    {
+        img: Five,
+        alt: `jordan-one-a-ma-maniere`
+    },
+    {
+        img: Six,
+        alt: `jordan-one-sail-cement`
+    },
+    {
+        img: Seven,
+        alt: `jordan-one-sail-cement-low`
+    },
+    {
+        img: Eight,
+        alt: `jordan-one-pine-green`
+    },
+    {
+        img: Nine,
+        alt: `jordan-one-mars-yard`
+    },
+    {
+        img: Ten,
+        alt: `jordan-one-ichiro`
+    },
+    {
+        img: Eleven,
+        alt: `anime-custom-panels`
+    }
+];
+
+export default lookBookData;
