2a1481233d6acab37c958bdd9b56b49adaed50bc
Justin <me@justinle.page>
2021-07-30 18:01:20 -0700

    updated contact form

--- client/src/components/FormBody/index.js
+++ client/src/components/FormBody/index.js
@@ -1,18 +1,54 @@
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import React from 'react';
 
 export default function FormBody() {
     const body = [
-        {
-            control: `name`,
-            label: `Name`,
-            invalid: `Please enter your name.`
-        },
-        {
-            control: `email`,
-            label: `Email`,
-            invalid: `Please enter your email.`
-        }
+        [
+            {
+                control: `name`,
+                label: `Name`,
+                invalid: `Please enter your name.`,
+                size: 8
+            },
+            {
+                control: `instagram`,
+                label: `Instagram`,
+                message: `@instagram`,
+                invalid: `Please enter your handle.`
+            }
+        ],
+        [
+            {
+                control: `email`,
+                label: `Email`,
+                invalid: `Please enter your email.`
+            }
+        ],
+        [
+            {
+                control: `shoe`,
+                label: `Base Shoe`,
+                message: `Please note if they are new, used, or need to be provided`,
+                invalid: `Please enter information about the base shoe.`,
+                size: 8
+            },
+            {
+                control: `size`,
+                label: `Size (US MEN'S)`
+            }
+        ],
+        [
+            {
+                control: `service`,
+                label: `Service`
+            },
+            {
+                control: `location`,
+                label: `Shipping`
+            }
+        ]
     ];
 
     const shoeSizes = [
@@ -39,34 +75,84 @@ export default function FormBody() {
         `Other`
     ];
 
+    const services = [`Aging`, `Custom`];
+
+    const shipping = [`USA`, `International`];
+
     return (
         <div className="form-body">
             {body.map((data, index) => (
-                <Form.Group controlId={data.control} key={`form-body-${index}`}>
-                    <Form.Label>
-                        <b>{data.label.toUpperCase()}</b>
-                    </Form.Label>
-                    {data.label === `Shoe Size (US MEN'S)` ? (
-                        <Form.Control required as="select">
-                            {shoeSizes.map((size, index) => (
-                                <option key={`shoe-size-${index}`}>
-                                    {size}
-                                </option>
-                            ))}
-                        </Form.Control>
-                    ) : (
-                        <>
-                            <Form.Control
-                                required
-                                type={data.label === `Email` ? `email` : `text`}
-                                placeholder={data.label}
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                {data.invalid}
-                            </Form.Control.Feedback>
-                        </>
-                    )}
-                </Form.Group>
+                <Row key={`form-row-${index}`}>
+                    {data.map((form, index) => (
+                        <Col
+                            xs={form.size}
+                            key={`${form.control}-col-${index}`}
+                        >
+                            <Form.Group
+                                controlId={form.control}
+                                key={`form-group-${index}`}
+                            >
+                                <Form.Label>
+                                    <b>{form.label.toUpperCase()}</b>
+                                </Form.Label>
+                                {form.control === `size` ||
+                                form.label === `Service` ||
+                                form.label === `Shipping` ? (
+                                    <Form.Control required as="select">
+                                        {form.control === `size`
+                                            ? shoeSizes.map((size, index) => (
+                                                  <option
+                                                      value={`${size}`}
+                                                      key={`select-size-${index}`}
+                                                  >
+                                                      {size}
+                                                  </option>
+                                              ))
+                                            : form.control === `service`
+                                            ? services.map((service, index) => (
+                                                  <option
+                                                      value={`${service}`}
+                                                      key={`select-service-${index}`}
+                                                  >
+                                                      {service}
+                                                  </option>
+                                              ))
+                                            : shipping.map(
+                                                  (location, index) => (
+                                                      <option
+                                                          value={`${location}`}
+                                                          key={`select-location-${index}`}
+                                                      >
+                                                          {location}
+                                                      </option>
+                                                  )
+                                              )}
+                                    </Form.Control>
+                                ) : (
+                                    <>
+                                        <Form.Control
+                                            required
+                                            type={
+                                                form.label === `Email`
+                                                    ? `email`
+                                                    : `text`
+                                            }
+                                            placeholder={
+                                                !form.message
+                                                    ? form.label
+                                                    : form.message
+                                            }
+                                            key={`${form.control}-${index}`}
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            {form.invalid}
+                                        </Form.Control.Feedback>
+                                    </>
+                                )}
+                            </Form.Group>
+                        </Col>
+                    ))}
+                </Row>
             ))}
         </div>
     );
