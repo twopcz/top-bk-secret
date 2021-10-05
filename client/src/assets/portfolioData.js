5ef7ab40bcb4c37c737219b709946019f0fb20ac
Justin <me@justinle.page>
2021-10-05 14:58:04 -0700

    portfolio image export

---
+++ client/src/assets/portfolioData.js
@@ -0,0 +1,115 @@
+import First from './portfolio_1005/1.jpg';
+import Second from './portfolio_1005/2.jpg';
+import Third from './portfolio_1005/3.jpg';
+import Fourth from './portfolio_1005/4.jpg';
+import Fifth from './portfolio_1005/5.jpg';
+import Sixth from './portfolio_1005/6.jpg';
+import Seventh from './portfolio_1005/7.jpg';
+import Eighth from './portfolio_1005/8.jpg';
+import Ninth from './portfolio_1005/9.jpg';
+import Tenth from './portfolio_1005/10.jpg';
+import Eleventh from './portfolio_1005/11.jpeg';
+import Twelfth from './portfolio_1005/12.jpg';
+import Thirteenth from './portfolio_1005/13.jpeg';
+import Fourteenth from './portfolio_1005/14.jpeg';
+import Fifteenth from './portfolio_1005/15.jpg';
+import Sixteenth from './portfolio_1005/16.jpeg';
+import Seventeenth from './portfolio_1005/17.JPG';
+import Eighteenth from './portfolio_1005/18.jpeg';
+import Nineteenth from './portfolio_1005/19.jpeg';
+import Twentieth from './portfolio_1005/20.jpg';
+import TwentyFirst from './portfolio_1005/21.JPG';
+import TwentySecond from './portfolio_1005/22.jpeg';
+
+const portfolio = [
+    {
+        img: First,
+        alt: `shadow-royal-unla-ones`
+    },
+    {
+        img: Second,
+        alt: `nothing-was-the-off-white-dunks`
+    },
+    {
+        img: Third,
+        alt: `unc-travis-ones`
+    },
+    {
+        img: Fourth,
+        alt: `vhulture-off-wbite-dunks`
+    },
+    {
+        img: Fifth,
+        alt: `aged-shadow-ones`
+    },
+    {
+        img: Sixth,
+        alt: `aged-unc-ones`
+    },
+    {
+        img: Seventh,
+        alt: `sonic-ones`
+    },
+    {
+        img: Eighth,
+        alt: `aged-panda-ones`
+    },
+    {
+        img: Ninth,
+        alt: `murakami-lv`
+    },
+    {
+        img: Tenth,
+        alt: `lakers-unla-ones`
+    },
+    {
+        img: Eleventh,
+        alt: `aged-bred-toe-ones`
+    },
+    {
+        img: Twelfth,
+        alt: `cememt-low-ones`
+    },
+    {
+        img: Thirteenth,
+        alt: `snakeskin-swoosh`
+    },
+    {
+        img: Fourteenth,
+        alt: `aged-unc-ones`
+    },
+    {
+        img: Fifteenth,
+        alt: `kaws-vans`
+    },
+    {
+        img: Sixteenth,
+        alt: `aged-fire-red-fours`
+    },
+    {
+        img: Seventeenth,
+        alt: `laker-threes`
+    },
+    {
+        img: Eighteenth,
+        alt: `goku-zoom-freaks`
+    },
+    {
+        img: Nineteenth,
+        alt: `blood-in-the-leaves-yeezy-700`
+    },
+    {
+        img: Twentieth,
+        alt: `rose-vans`
+    },
+    {
+        img: TwentyFirst,
+        alt: `kobe-mural`
+    },
+    {
+        img: TwentySecond,
+        alt: `laker-unla-ones-yellow`
+    }
+];
+
+export default portfolio;
