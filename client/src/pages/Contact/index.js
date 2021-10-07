793c324e7618daf0780e91d136072f613913b024
Justin <me@justinle.page>
2021-10-06 18:34:13 -0700

    max vh on profile pic

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -7,9 +7,13 @@ export default function Contact() {
     return (
         <Col className="my-auto d-flex flex-column flex-md-row justify-content-center pt-5 pt-md-0 px-0">
             <Col md={4} className="text-center mx-auto px-0">
-                <img src={Profile} className="img-fluid" alt="profile"></img>
+                <img
+                    src={Profile}
+                    className="img-fluid max-vh-100"
+                    alt="profile"
+                ></img>
             </Col>
-            <Col md={6} className="mx-auto my-auto">
+            <Col md={6} className="mx-auto my-5 my-md-auto">
                 <ContactForm />
             </Col>
         </Col>
